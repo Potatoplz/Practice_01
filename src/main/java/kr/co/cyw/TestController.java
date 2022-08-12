a12a22eb7ad6eedcd0ca83d93f4ea62b606d6948
Potatoplz <beneh0710@gmail.com>
2022-08-12 11:46:26 +0900

    메인헤더 수정 중

--- src/main/java/kr/co/cyw/TestController.java
+++ src/main/java/kr/co/cyw/TestController.java
@@ -7,13 +7,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class TestController {
 	
-	  @RequestMapping(value = "/home", method = RequestMethod.GET) 
+	  @RequestMapping(value = "/paybtn", method = RequestMethod.GET) 
 	  public String test() { 
 		  return "/kakaopay/kakaopay";//jsp파일명
 	  }
 	  @RequestMapping(value = "/header", method = RequestMethod.GET) 
 	  public String header() { 
 		  return "/header";//jsp파일명
+	  }
+	  @RequestMapping(value = "/home", method = RequestMethod.GET) 
+	  public String home() { 
+		  return "/home0";//jsp파일명
 	  }
 	 
 }
